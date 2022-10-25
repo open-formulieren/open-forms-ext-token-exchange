b0982877cca4bcad36504f000c81a0a3c046e09b
SilviaAmAm <silvia@maykinmedia.nl>
2022-10-25 17:28:02 +0200

    :sparkles: [open-formulieren/open-forms#2197] Add plugin and auth class

--- token_exchange/plugin.py
+++ token_exchange/plugin.py
@@ -1,9 +1,10 @@
 from openforms.pre_requests.base import PreRequestHookBase
 from openforms.pre_requests.registry import register
 
+from .auth import TokenAccessAuth
+
 
 @register("token_exchange")
 class TokenExchangePreRequestHook(PreRequestHookBase):
     def __call__(self, method: str, url: str, **kwargs):
-        # TODO
-        pass
+        kwargs["auth"] = TokenAccessAuth
---
+++ token_exchange/signals.py
@@ -0,0 +1,27 @@
+import threading
+
+from django.core import signals
+from django.dispatch import receiver
+
+from openforms.authentication.signals import authentication_success
+from rest_framework.request import Request
+
+storage = threading.local()
+
+
+@receiver(authentication_success, dispatch_uid="token_exchange.extract_access_token")
+def extract_access_token(sender, request: Request, **kwargs) -> None:
+    # TODO Extract token from request
+    access_token = "some extracted token"
+    id_token = "some extracted id token"
+
+    storage.access_token = access_token
+    storage.id_token = id_token  # TODO not sure if needed yet
+
+
+def clear_tokens(**kwargs):
+    storage.access_token = None
+    storage.id_token = None
+
+
+signals.request_finished.connect(clear_tokens)
