9806b9ce451ee0fbb61538b07c450b43a5cdf59b
SilviaAmAm <silvia@maykinmedia.nl>
2022-10-26 15:35:37 +0200

    :construction: [open-formulieren/open-forms#2197] Updated model

--- token_exchange/models.py
+++ token_exchange/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from zgw_consumers.constants import APITypes
+import requests
+from furl import furl
+
+OPEN_ID_CONFIG_PATH = ".well-known/openid-configuration"
 
 
 class TokenExchangeConfiguration(models.Model):
     service = models.OneToOneField(
         to="zgw_consumers.Service",
-        verbose_name=_("Token exchange service"),
-        on_delete=models.PROTECT,
-        limit_choices_to={"api_type": APITypes.orc},
-        related_name="+",
-        null=True,
+        verbose_name=_("service"),
+        help_text=_("External API service"),
+        on_delete=models.CASCADE,
     )
     audience = models.CharField(
-        name="audience",
         verbose_name=_("audience"),
         help_text=_(
             "Specifies the scope/audience, so that Keycloak knows which sort of access token to return."
@@ -23,24 +23,39 @@ class TokenExchangeConfiguration(models.Model):
         max_length=250,
     )
     client_id = models.CharField(
-        name="client id",
-        verbose_name=_("Client ID"),
+        verbose_name=_("client ID"),
         help_text=_("Keycloak client ID."),
         blank=False,
         max_length=250,
     )
     secret = models.CharField(
-        name="secret",
         verbose_name=_("secret"),
         help_text=_("Keycloak secret for the client ID specified."),
         blank=False,
         max_length=250,
     )
-    keycloak_base_url = models.URLField(
-        name="keycloak base URL",
-        verbose_name=_("keycloak base URL"),
+    discovery_endpoint = models.URLField(
+        verbose_name=_("discovery endpoint"),
+        help_text=_(
+            "URL of your OpenID Connect provider discovery endpoint ending with a slash "
+            "(`.well-known/...` will be added automatically). "
+            "If this is provided, the remaining endpoints can be omitted, as "
+            "they will be derived from this endpoint."
+        ),
         blank=False,
     )
 
     class Meta:
         verbose_name = _("Token exchange plugin configuration")
+
+    _token_exchange_endpoint = None
+
+    @property
+    def token_exchange_endpoint(self):
+        if not self._token_exchange_endpoint:
+            discovery_endpoint = furl(self.discovery_endpoint)
+            discovery_endpoint /= OPEN_ID_CONFIG_PATH
+            response = requests.get(discovery_endpoint.url, timeout=10)
+            configuration = response.json()
+            self._token_exchange_endpoint = configuration["token_endpoint"]
+        return self._token_exchange_endpoint
