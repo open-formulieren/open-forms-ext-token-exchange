554ed5b4cd3945cdda54f752000f2c341b4cca94
SilviaAmAm <silvia@maykinmedia.nl>
2022-10-27 14:17:37 +0200

    :white_check_mark: [open-formulieren/open-forms#2197] Test signal receiver

--- token_exchange/tests/test_signals.py
+++ token_exchange/tests/test_signals.py
@@ -1,13 +1,60 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
+from openforms.authentication.constants import FORM_AUTH_SESSION_KEY
+from rest_framework.test import APIRequestFactory
 
-# TODO
+from ..signals import clear_storage, extract_access_token
+
+factory = APIRequestFactory()
+
+
 class SignalReceiverTests(TestCase):
     def test_extract_access_token(self):
-        pass
+        request = factory.get("/foo")
+        request.session = {
+            FORM_AUTH_SESSION_KEY: {
+                "plugin": "plugin1",
+                "attribute": "bsn",
+                "value": "123",
+                "access_token": "some-token",
+            }
+        }
+
+        with patch(
+            "token_exchange.signals.storage", access_token=None, plugin=None
+        ) as m_storage:
+            extract_access_token(sender="test", request=request)
+
+            self.assertEqual("plugin1", m_storage.plugin)
+            self.assertEqual("some-token", m_storage.access_token)
 
     def test_extract_access_token_no_token(self):
-        pass
+        request = factory.get("/foo")
+        request.session = {
+            FORM_AUTH_SESSION_KEY: {
+                "plugin": "plugin1",
+                "attribute": "bsn",
+                "value": "123",
+            }
+        }
+
+        with patch(
+            "token_exchange.signals.storage", access_token=None, plugin=None
+        ) as m_storage:
+            extract_access_token(sender="test", request=request)
+
+            self.assertIsNone(m_storage.plugin)
+            self.assertIsNone(m_storage.access_token)
 
     def test_clear_access_token(self):
-        pass
+        with patch(
+            "token_exchange.signals.storage",
+            access_token="some-token",
+            plugin="plugin1",
+        ) as m_storage:
+            clear_storage()
+
+            self.assertIsNone(m_storage.plugin)
+            self.assertIsNone(m_storage.access_token)
